bcf462360638158673197f03579833bf9896cc89
Erin K <edkeeffe@gmail.com>
2019-02-24 17:50:33 -0500

    update hompage with new logo, image, rsvp form

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,6 +6,7 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 import './all.sass'
+import './glu.scss'
 
 const TemplateWrapper = ({ children }) => (
   <StaticQuery
